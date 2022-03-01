b74b99b625b898e02f374f9a138147bcd1c75b3f
Saatvik Arya <aryasaatvik@gmail.com>
2022-02-28 18:02:47 -0800

    firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGP9fkPk1EfT-bl8O2BqJ627Hy6zYqPwo",
+  authDomain: "serendipity-2f854.firebaseapp.com",
+  projectId: "serendipity-2f854",
+  storageBucket: "serendipity-2f854.appspot.com",
+  messagingSenderId: "305536146214",
+  appId: "1:305536146214:web:e74548a840696158967fe7"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
