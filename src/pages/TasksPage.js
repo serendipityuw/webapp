1418412b62824d4a2357afec09340f827a79be79
Saatvik Arya <aryasaatvik@gmail.com>
2022-03-08 19:26:40 -0800

    tasks

--- src/pages/TasksPage.js
+++ src/pages/TasksPage.js
@@ -7,20 +7,19 @@ import { AuthContext } from "../context";
 
 function TasksPage(props) {
     const [cards, setCards] = useState([]);
-    const { data, tasks } = useContext(AuthContext);
+    const { tasks } = useContext(AuthContext);
 
     useEffect(() => {
-        if (tasks && data && data.tasks) {
-            const newTasks = [];
-            
+        if (tasks) {
+            const newTasks = [];            
             Object.keys(tasks).forEach(task_id => {
-                if (data.tasks.indexOf(task_id) === -1 && tasks[task_id].status === "Unclaimed") {
+                if (tasks[task_id].status === "Unclaimed") {
                     newTasks.push( <Task key={task_id} id={task_id} task={tasks[task_id]} />);
                 }
             });
             setCards(newTasks);
         }
-    }, [tasks, data]);
+    }, [tasks]);
 
     return (
         <section className="tasks">
